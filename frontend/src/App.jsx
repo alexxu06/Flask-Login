b26380e7e16c74f22bdae0bd6c22482c48c83fab
alexxu06 <alexxu0308@gmail.com>
2025-02-22 23:35:06 -0800

    Updated login page of README file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
   return (
-    <div className="wrapper">
-        <h1>Login</h1>
-        <div className="input-box">
-            <p>Username/Email</p>
-            <input type="text" placeholder="Enter Username or Email"/>
-        </div>
-        <div className="input-box">
-            <p>Password</p>
-            <input type="password" placeholder="Enter Password"/>
-        </div>
-        <div className="buttons">
-            <button>Login</button>
-            <p>Sign Up</p>
-        </div>
+    <div className="outer-wrapper">
+        <LoginPage />
     </div>
   )
 }
